83d0694f7bdb0bc2fa15f8db94922d42931aab8b
jiuduweiliangeer <64030931@qq.com>
2019-11-20 17:15:08 +0800

    StudyDemo

---
+++ src/Java/LeetCode/LinkTwoSum.java
@@ -0,0 +1,24 @@
+package Java.LeetCode;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkTwoSum{
+    /*public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){val=x;}
+    }*/
+    public static void main(String []args){
+        List<Integer> list=new LinkedList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        Iterator<Integer> iterator=list.iterator();
+        while(iterator.hasNext()){
+            System.out.print(iterator.next());
+        }
+        System.out.print(list);
+    }
+}
--- src/Java/LeetCode/TwoSum.java
+++ src/Java/LeetCode/TwoSum.java
@@ -8,14 +8,14 @@ public class TwoSum {
         Map<Integer,Integer> map=new HashMap<Integer,Integer>();
         for (int i = 0; i <nums.length ; i++) {
             map.put(nums[i],i);
-        }
+        }//将所有数据放入hashmap
         for (int i = 0; i <nums.length ; i++) {
             int complement=target-nums[i];
             if(map.containsKey(complement)&&map.get(complement)!=i){
                 int key=map.get(complement);
                 return new int[]{i,key};
             }
-        }
+        }//通过hashmap中的contain方法来检测是否存在这个值，降低时间复杂度
         throw new IllegalArgumentException("no");
     }
 }

96d736cdf25b8c758629f29b611b279807889e12
jiuduweiliangeer <64030931@qq.com>
2019-11-20 19:53:38 +0800

    StudyDemo

--- src/Java/LeetCode/_1_TwoSum.java
+++ src/Java/LeetCode/_1_TwoSum.java
@@ -3,7 +3,7 @@ package Java.LeetCode;
 import java.util.HashMap;
 import java.util.Map;
 
-public class _1TwoSum {
+public class _1_TwoSum {
     public int[] twoSum(int [] nums, int target){
         Map<Integer,Integer> map=new HashMap<Integer,Integer>();
         for (int i = 0; i <nums.length ; i++) {
--- src/Java/LeetCode/_2_LinkTwoSum.java
+++ src/Java/LeetCode/_2_LinkTwoSum.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-public class _2LinkTwoSum {
+public class _2_LinkTwoSum {
     /*public class ListNode{
         int val;
         ListNode next;

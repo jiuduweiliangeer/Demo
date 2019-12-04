d89100b007bd99e5046b7bcd5ee441f7597087e4
jiuduweiliangeer <64030931@qq.com>
2019-12-04 16:50:50 +0800

    StudyDemo

---
+++ src/Java/LeetCode/SpecificDemo.java
@@ -0,0 +1,9 @@
+package Java.LeetCode;
+
+public class SpecificDemo {
+    public static void main(String []args){
+        _5_CurrentlongestPalindrome currentlongestPalindrome=new _5_CurrentlongestPalindrome();
+        String s=currentlongestPalindrome.solutin("acbcncbb");
+        System.out.println(s);
+    }
+}
---
+++ src/Java/LeetCode/_5_CurrentlongestPalindrome.java
@@ -0,0 +1,49 @@
+package Java.LeetCode;
+
+public class _5_CurrentlongestPalindrome {
+    public String solutin(String s){
+        if(s.length()<2){
+            return s;
+        }
+        int start=0;
+        int maxLine=1;
+        int len=s.length();
+        int slen=len*2+1;
+        String tempstring=addBunder(s,'#');
+        for (int i = 0; i <slen ; i++) {
+            int step=step(tempstring,i);
+            if(step>maxLine){
+                maxLine=step;
+                start=(i-maxLine)/2;
+            }
+        }
+        return s.substring(start,start+maxLine);//字符串方法，切割返回
+    }
+    public int step(String s,int center){
+        int steptemp=0;
+        int left=center-1;
+        int right=center+1;
+        int len=s.length();
+        while(left>=0&&right<len&&s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
+            steptemp++;
+        }
+        return steptemp;
+    }//中心扩散判断步长
+    public String addBunder(String s,char divide){
+        if(s.length()==0){
+            return "";
+        }
+        if(s.indexOf(divide)!=-1){
+            throw new IllegalArgumentException("传入参数错误");
+        }
+        StringBuilder stringBuilder=new StringBuilder();
+        for (int i = 0; i <s.length() ; i++) {
+            stringBuilder.append(divide);
+            stringBuilder.append(s.charAt(i));
+        }
+        stringBuilder.append(divide);
+        return stringBuilder.toString();
+    }//插入建个符，准确判断步长
+}

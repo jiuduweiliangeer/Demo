82caac2718efaa77dec96bede75b4cd12e3a229a
jiuduweiliangeer <64030931@qq.com>
2019-12-06 15:46:42 +0800

    StudyDemo

--- src/Java/LeetCode/SpecificDemo.java
+++ src/Java/LeetCode/SpecificDemo.java
@@ -5,8 +5,11 @@ public class SpecificDemo {
         _5_CurrentlongestPalindrome currentlongestPalindrome=new _5_CurrentlongestPalindrome();
         String s=currentlongestPalindrome.solutin("acbcncbb");
         System.out.println(s);*/
-        _6_convert convert=new _6_convert();
+/*        _6_convert convert=new _6_convert();
         String s=convert.convert("LEETCODEISHIRING",4);
+        System.out.println(s);*/
+        _8_myatoi myatoi=new _8_myatoi();
+        int s=myatoi.myatoi("    -88827   5655  U");
         System.out.println(s);
     }
 }
